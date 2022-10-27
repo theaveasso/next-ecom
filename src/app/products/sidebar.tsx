05ad5f6080f222d2318049e1a75a78544eab59d3
theaveasso <theaveras.so@gmail.com>
2022-10-27 11:44:04 +0700

    test

--- src/app/products/sidebar.tsx
+++ src/app/products/sidebar.tsx
@@ -2,7 +2,7 @@ const Sidebar = () => {
 	return (
 		<div className='h-[800px] mt-10 flex-shrink-0 rounded-lg w-[200px] bg-gradient-to-br bg-opacity-0 from-black-600 to-transparent p-5 divide-y-[1px] divide-gray-400'>
 			<h2 className='font-semibold p-2'>Home</h2>
-			<h2 className='py-2'>tetrs</h2>
+			<h2 className='py-2'>Test</h2>
 		</div>
 	);
 };
